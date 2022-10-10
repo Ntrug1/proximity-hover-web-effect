0802dd2baf063693412801040e19ef1e64768071
Ntrug1 <quenpassroi1@gmail.com>
2022-10-10 11:28:14 +0700

    init image and eye png (need to create different eyes for each character)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import eye from "./eye.png";
+import mainImage from "./pickle_rick.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div className="image-wrapper">
+      <img width="800px" height="700px" alt="rick-and-morty" src={mainImage} />
+      <div className="eyes">
+        <img alt="eye" src={eye} />
+        <img alt="eye" src={eye} />
+        <img alt="eye" src={eye} />
+        <img alt="eye" src={eye} />
+      </div>
     </div>
   );
 }
